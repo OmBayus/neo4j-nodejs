fcfe18435d2b7a29e0b600ba3435cef8b8635db2
OmBayus <omerbayramcavus@gmail.com>
2022-05-07 15:08:11 +0300

    movie service

--- src/services/movie.service.js
+++ src/services/movie.service.js
@@ -4,7 +4,7 @@ import { toNativeTypes } from '../utils.js'
 import NotFoundError from '../errors/not-found.error.js'
 
 // TODO: Import the `int` function from neo4j-driver
-
+import { int } from 'neo4j-driver'
 export default class MovieService {
   /**
    * @type {neo4j.Driver}
@@ -40,11 +40,24 @@ export default class MovieService {
   // tag::all[]
   async all(sort = 'title', order = 'ASC', limit = 6, skip = 0, userId = undefined) {
     // TODO: Open an Session
+    const session = this.driver.session()
     // TODO: Execute a query in a new Read Transaction
+    const res = await session.readTransaction(tx => tx.run(`
+      MATCH (m:Movie)
+      WHERE m.\`${sort}\` IS NOT NULL
+      RETURN m { .* } AS movie
+      ORDER BY m.\`${sort}\` ${order}
+      SKIP $skip
+      LIMIT $limit
+    `, { skip: int(skip), limit: int(limit) }))
     // TODO: Get a list of Movies from the Result
+    const movies = res.records.map(row => toNativeTypes(row.get('movie')))
     // TODO: Close the session
+    await session.close()
 
-    return popular
+
+
+    return movies
   }
   // end::all[]
 
